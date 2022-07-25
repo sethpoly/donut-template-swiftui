9c941bbbe5c3c4c8190c5f3bdbd46cee72b0a32a
Seth Polyniak <sethpolyniak@sethpoly-macbookair.mynetworksettings.com>
2022-07-25 19:21:26 -0400

    first commit

---
+++ Shared/DonutTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutTemplateApp.swift
+//  Shared
+//
+//  Created by Seth Polyniak on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DonutTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
