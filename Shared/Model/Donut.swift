d9659aafbd972b3dccf2fc2d9fb0bf78c91d817c
Seth Polyniak <emailspolyniak@gmail.com>
2022-07-27 08:55:40 -0400

    [feat/#3/header] - add placeholder donut model

---
+++ Shared/Model/Donut.swift
@@ -0,0 +1,15 @@
+//
+//  Donut.swift
+//  DonutTemplate
+//
+//  Created by Seth Polyniak on 7/27/22.
+//
+
+import Foundation
+import UIKit
+
+struct Donut {
+    let name: String
+    let price: String
+    let imageName: String
+}
