d51e61a4bc8ea4dfe6a1ab3ca2b8cdb12383ef92
Seth Polyniak <emailspolyniak@gmail.com>
2022-08-23 08:54:25 -0400

    [feat/5/total] - add payment summary card

--- Shared/View/CartView.swift
+++ Shared/View/CartView.swift
@@ -19,34 +19,61 @@ struct CartView: View {
 private struct CartViewContent: View {
     let items: [Donut]
     var body: some View {
-        ZStack {
-            VStack {
-                CartHeader(
-                    title: "My Cart",
-                    subtitle: "# items"
-                )
-                
-                ScrollView {
-                    LazyVStack {
-                        ForEach(items.indices, id: \.self) { index in
-                            CartItem(
-                                onDeleteClick: {
-                                    /**TODO: */
-                                }
-                            )
-                            CustomDivider(
-                                color: Color.onBackgroundVariantLight
-                            )
+        GeometryReader { metrics in
+            ZStack {
+                VStack {
+                    CartHeader(
+                        title: "My Cart",
+                        subtitle: "\(items.count) items"
+                    )
+                    ScrollView {
+                        // Donut list
+                        LazyVStack {
+                            ForEach(items.indices, id: \.self) { index in
+                                CartItem(
+                                    onDeleteClick: {
+                                        /**TODO: */
+                                    }
+                                )
+                                CustomDivider(
+                                    color: Color.onBackgroundVariantLight
+                                )
+                            }
                         }
                     }
+                    .frame(maxHeight: metrics.size.height * 0.50)
+                    
+                    Spacer()
+                    
+                    // Payment summary card
+                    SubtotalSummaryCard(
+                        subtotalPrice: "$9.99",
+                        deliveryPrice: "$0.00",
+                        totalPrice: "$9.99"
+                    )
+                        
+                    Spacer()
+                    
+                    // Bottom cart buttons/info
+                    HStack {
+                        // Add to cart
+                        AccentButton(
+                            text: "Continue to checkout",
+                            onClick: {}
+                        )
+                    }
+                    .frame(maxWidth: .infinity,  alignment: .leading)
+                    .background(Color.background)
+                    
+                    Spacer()
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .padding(PaddingManager.view)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-            .padding(PaddingManager.view)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
+            .ignoresSafeArea(.all, edges: .bottom)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.background)
-        .ignoresSafeArea(.all, edges: .bottom)
     }
 }
 
@@ -164,8 +191,66 @@ private struct BorderedImageButton: View {
     }
 }
 
+private struct SubtotalSummaryCard: View {
+    let subtotalPrice: String
+    let deliveryPrice: String
+    let totalPrice: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Subtotal Summary")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                SubtotalItemEntry(itemName: "Subtotal", itemPrice: subtotalPrice)
+                SubtotalItemEntry(itemName: "Delivery", itemPrice: deliveryPrice)
+            }
+            CustomDivider(
+                color: Color.onBackgroundVariantLight
+            )
+            HStack {
+                Text("Subtotal")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                Spacer()
+                Text(totalPrice)
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+        }
+        .padding()
+        .background(Color.foreground)
+        .cornerRadius(CGFloat.medium)
+        .shadow(color: .gray, radius: 3, x: -2, y: 2)
+    }
+}
+
+private struct SubtotalItemEntry: View {
+    let itemName: String
+    let itemPrice: String
+    var font: Font = .callout
+    var weight: Font.Weight = .medium
+    var color: Color = Color.onBackgroundVariant
+    
+    var body: some View {
+        HStack {
+            Text(itemName)
+                .font(font)
+                .fontWeight(weight)
+                .foregroundColor(color)
+            Spacer()
+            Text(itemPrice)
+                .font(font)
+                .fontWeight(weight)
+                .foregroundColor(color)
+        }
+    }
+}
+
 struct Cart_Previews: PreviewProvider {
     static var donuts: [Donut] = [
+        Donut(name: "Strawberry", price: "$2.00", imageName: "donutStrawberry"),
+        Donut(name: "Strawberry", price: "$2.00", imageName: "donutStrawberry"),
         Donut(name: "Strawberry", price: "$2.00", imageName: "donutStrawberry"),
         Donut(name: "Strawberry", price: "$2.00", imageName: "donutStrawberry"),
         Donut(name: "Strawberry", price: "$2.00", imageName: "donutStrawberry"),
