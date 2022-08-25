28e433dea7eebcb281b03ae02c9a8da054743db6
Seth Polyniak <emailspolyniak@gmail.com>
2022-08-25 07:59:28 -0400

    [feat/10/donutDetailView] - add nav buttons to donutDetailView

---
+++ Shared/Component/RoundButton.swift
@@ -0,0 +1,23 @@
+//
+//  RoundButton.swift
+//  DonutTemplate
+//
+//  Created by Seth Polyniak on 8/25/22.
+//
+
+import SwiftUI
+
+struct RoundButton<Content: View>: View {
+    let onClick: () -> Void
+    let backgroundColor: Color = Color.white
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        Button(action: onClick) {
+            content
+        }
+        .padding(6)
+        .background(backgroundColor)
+        .cornerRadius(6)
+    }
+}
